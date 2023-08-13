ec137e15718e32ec816081f3d18b1f16c368a389
vfaust <vl.averkov@mail.ru>
2023-08-13 08:06:10 -0400

    move schedule update to define\'s

--- src/hash/sha256.cpp
+++ src/hash/sha256.cpp
@@ -17,6 +17,30 @@
 
 # define s1(x) (ROTR((x), 17) ^ ROTR((x), 19) ^ (x >> 10))
 
+# define Round0_15(i) \
+    Schedule[i] = static_cast<std::uint32_t>(From[i]) << 24 | static_cast<std::uint32_t>(From[i + 1]) << 16 | static_cast<std::uint32_t>(From[i + 2]) << 8 | static_cast<std::uint32_t>(From[i + 3]); \
+    tmp1 = h + S1(e) + ch(e, f, g) + Constants[i] + Schedule[i]; \
+    tmp2 = S0(a) + maj(a, b, c); \
+    h = g; g = f; f = e; \
+    e = d + tmp1; d = c; \
+    c = b; b = a; \
+    a = tmp1 + tmp2; \
+
+# define Round16_63(i) \
+    Schedule[i] = s1(Schedule[i - 2]) + Schedule[i - 7] + s0(Schedule[i - 15]) + Schedule[i - 16]; \
+    tmp1 = h + S1(e) + ch(e, f, g) + Constants[i] + Schedule[i]; \
+    tmp2 = S0(a) + maj(a, b, c); \
+    h = g; g = f; f = e; \
+    e = d + tmp1; d = c; \
+    c = b; b = a; \
+    a = tmp1 + tmp2; \
+
+# define Concat4_8_32(target, a, b, c, d) \
+    target = static_cast<decltype(target)>(a) << 24 \
+    | static_cast<decltype(target)>(b) << 16 \
+    | static_cast<decltype(target)>(c) << 8 \
+    | static_cast<decltype(target)>(d); \
+
 constexpr static std::uint32_t Initial[] = {
     0x6A09E667, 0xBB67AE85,
     0x3C6EF372, 0xA54FF53A,
@@ -35,75 +59,21 @@ constexpr static std::uint32_t Constants[] = {
     0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
 };
 
+template<typename Type>
+[[ nodiscard ]] std::string to_binary(Type Data) noexcept
+{
+    std::string Result = "";
 
-static void update (std::uint32_t* Result, const std::uint8_t* From) noexcept
+    std::uint64_t CurrentBit = 8 * sizeof Data;
+    while(CurrentBit--)
+        Result += ((Data >> CurrentBit) & 1) == 0 ? "0" : "1";
+
+    return Result;
+}
+
+static void update(std::uint32_t* Result, const std::uint8_t* From) noexcept
 {
     static std::uint32_t Schedule[64]; 
-
-    Schedule[0] = static_cast<std::uint32_t>(From[0]) << 24 | static_cast<std::uint32_t>(From[1]) << 16 | static_cast<std::uint32_t>(From[2]) << 8 | static_cast<std::uint32_t>(From[3]);
-    Schedule[1] = static_cast<std::uint32_t>(From[4]) << 24 | static_cast<std::uint32_t>(From[5]) << 16 | static_cast<std::uint32_t>(From[6]) << 8 | static_cast<std::uint32_t>(From[7]);
-    Schedule[2] = static_cast<std::uint32_t>(From[8]) << 24 | static_cast<std::uint32_t>(From[9]) << 16 | static_cast<std::uint32_t>(From[10]) << 8 | static_cast<std::uint32_t>(From[11]);
-    Schedule[3] = static_cast<std::uint32_t>(From[12]) << 24 | static_cast<std::uint32_t>(From[13]) << 16 | static_cast<std::uint32_t>(From[14]) << 8 | static_cast<std::uint32_t>(From[15]);
-    Schedule[4] = static_cast<std::uint32_t>(From[16]) << 24 | static_cast<std::uint32_t>(From[17]) << 16 | static_cast<std::uint32_t>(From[18]) << 8 | static_cast<std::uint32_t>(From[19]);
-    Schedule[5] = static_cast<std::uint32_t>(From[20]) << 24 | static_cast<std::uint32_t>(From[21]) << 16 | static_cast<std::uint32_t>(From[22]) << 8 | static_cast<std::uint32_t>(From[23]);
-    Schedule[6] = static_cast<std::uint32_t>(From[24]) << 24 | static_cast<std::uint32_t>(From[25]) << 16 | static_cast<std::uint32_t>(From[26]) << 8 | static_cast<std::uint32_t>(From[27]);
-    Schedule[7] = static_cast<std::uint32_t>(From[28]) << 24 | static_cast<std::uint32_t>(From[29]) << 16 | static_cast<std::uint32_t>(From[30]) << 8 | static_cast<std::uint32_t>(From[31]);
-    Schedule[8] = static_cast<std::uint32_t>(From[32]) << 24 | static_cast<std::uint32_t>(From[33]) << 16 | static_cast<std::uint32_t>(From[34]) << 8 | static_cast<std::uint32_t>(From[35]);
-    Schedule[9] = static_cast<std::uint32_t>(From[36]) << 24 | static_cast<std::uint32_t>(From[37]) << 16 | static_cast<std::uint32_t>(From[38]) << 8 | static_cast<std::uint32_t>(From[39]);
-    Schedule[10] = static_cast<std::uint32_t>(From[40]) << 24 | static_cast<std::uint32_t>(From[41]) << 16 | static_cast<std::uint32_t>(From[42]) << 8 | static_cast<std::uint32_t>(From[43]);
-    Schedule[11] = static_cast<std::uint32_t>(From[44]) << 24 | static_cast<std::uint32_t>(From[45]) << 16 | static_cast<std::uint32_t>(From[46]) << 8 | static_cast<std::uint32_t>(From[47]);
-    Schedule[12] = static_cast<std::uint32_t>(From[48]) << 24 | static_cast<std::uint32_t>(From[49]) << 16 | static_cast<std::uint32_t>(From[50]) << 8 | static_cast<std::uint32_t>(From[51]);
-    Schedule[13] = static_cast<std::uint32_t>(From[52]) << 24 | static_cast<std::uint32_t>(From[53]) << 16 | static_cast<std::uint32_t>(From[54]) << 8 | static_cast<std::uint32_t>(From[55]);
-    Schedule[14] = static_cast<std::uint32_t>(From[56]) << 24 | static_cast<std::uint32_t>(From[57]) << 16 | static_cast<std::uint32_t>(From[58]) << 8 | static_cast<std::uint32_t>(From[59]);
-    Schedule[15] = static_cast<std::uint32_t>(From[60]) << 24 | static_cast<std::uint32_t>(From[61]) << 16 | static_cast<std::uint32_t>(From[62]) << 8 | static_cast<std::uint32_t>(From[63]);
-    Schedule[16] = s1(Schedule[14]) + Schedule[9] + s0(Schedule[1]) + Schedule[0];
-    Schedule[17] = s1(Schedule[15]) + Schedule[10] + s0(Schedule[2]) + Schedule[1];
-    Schedule[18] = s1(Schedule[16]) + Schedule[11] + s0(Schedule[3]) + Schedule[2];
-    Schedule[19] = s1(Schedule[17]) + Schedule[12] + s0(Schedule[4]) + Schedule[3];
-    Schedule[20] = s1(Schedule[18]) + Schedule[13] + s0(Schedule[5]) + Schedule[4];
-    Schedule[21] = s1(Schedule[19]) + Schedule[14] + s0(Schedule[6]) + Schedule[5];
-    Schedule[22] = s1(Schedule[20]) + Schedule[15] + s0(Schedule[7]) + Schedule[6];
-    Schedule[23] = s1(Schedule[21]) + Schedule[16] + s0(Schedule[8]) + Schedule[7];
-    Schedule[24] = s1(Schedule[22]) + Schedule[17] + s0(Schedule[9]) + Schedule[8];
-    Schedule[25] = s1(Schedule[23]) + Schedule[18] + s0(Schedule[10]) + Schedule[9];
-    Schedule[26] = s1(Schedule[24]) + Schedule[19] + s0(Schedule[11]) + Schedule[10];
-    Schedule[27] = s1(Schedule[25]) + Schedule[20] + s0(Schedule[12]) + Schedule[11];
-    Schedule[28] = s1(Schedule[26]) + Schedule[21] + s0(Schedule[13]) + Schedule[12];
-    Schedule[29] = s1(Schedule[27]) + Schedule[22] + s0(Schedule[14]) + Schedule[13];
-    Schedule[30] = s1(Schedule[28]) + Schedule[23] + s0(Schedule[15]) + Schedule[14];
-    Schedule[31] = s1(Schedule[29]) + Schedule[24] + s0(Schedule[16]) + Schedule[15];
-    Schedule[32] = s1(Schedule[30]) + Schedule[25] + s0(Schedule[17]) + Schedule[16];
-    Schedule[33] = s1(Schedule[31]) + Schedule[26] + s0(Schedule[18]) + Schedule[17];
-    Schedule[34] = s1(Schedule[32]) + Schedule[27] + s0(Schedule[19]) + Schedule[18];
-    Schedule[35] = s1(Schedule[33]) + Schedule[28] + s0(Schedule[20]) + Schedule[19];
-    Schedule[36] = s1(Schedule[34]) + Schedule[29] + s0(Schedule[21]) + Schedule[20];
-    Schedule[37] = s1(Schedule[35]) + Schedule[30] + s0(Schedule[22]) + Schedule[21];
-    Schedule[38] = s1(Schedule[36]) + Schedule[31] + s0(Schedule[23]) + Schedule[22];
-    Schedule[39] = s1(Schedule[37]) + Schedule[32] + s0(Schedule[24]) + Schedule[23];
-    Schedule[40] = s1(Schedule[38]) + Schedule[33] + s0(Schedule[25]) + Schedule[24];
-    Schedule[41] = s1(Schedule[39]) + Schedule[34] + s0(Schedule[26]) + Schedule[25];
-    Schedule[42] = s1(Schedule[40]) + Schedule[35] + s0(Schedule[27]) + Schedule[26];
-    Schedule[43] = s1(Schedule[41]) + Schedule[36] + s0(Schedule[28]) + Schedule[27];
-    Schedule[44] = s1(Schedule[42]) + Schedule[37] + s0(Schedule[29]) + Schedule[28];
-    Schedule[45] = s1(Schedule[43]) + Schedule[38] + s0(Schedule[30]) + Schedule[29];
-    Schedule[46] = s1(Schedule[44]) + Schedule[39] + s0(Schedule[31]) + Schedule[30];
-    Schedule[47] = s1(Schedule[45]) + Schedule[40] + s0(Schedule[32]) + Schedule[31];
-    Schedule[48] = s1(Schedule[46]) + Schedule[41] + s0(Schedule[33]) + Schedule[32];
-    Schedule[49] = s1(Schedule[47]) + Schedule[42] + s0(Schedule[34]) + Schedule[33];
-    Schedule[50] = s1(Schedule[48]) + Schedule[43] + s0(Schedule[35]) + Schedule[34];
-    Schedule[51] = s1(Schedule[49]) + Schedule[44] + s0(Schedule[36]) + Schedule[35];
-    Schedule[52] = s1(Schedule[50]) + Schedule[45] + s0(Schedule[37]) + Schedule[36];
-    Schedule[53] = s1(Schedule[51]) + Schedule[46] + s0(Schedule[38]) + Schedule[37];
-    Schedule[54] = s1(Schedule[52]) + Schedule[47] + s0(Schedule[39]) + Schedule[38];
-    Schedule[55] = s1(Schedule[53]) + Schedule[48] + s0(Schedule[40]) + Schedule[39];
-    Schedule[56] = s1(Schedule[54]) + Schedule[49] + s0(Schedule[41]) + Schedule[40];
-    Schedule[57] = s1(Schedule[55]) + Schedule[50] + s0(Schedule[42]) + Schedule[41];
-    Schedule[58] = s1(Schedule[56]) + Schedule[51] + s0(Schedule[43]) + Schedule[42];
-    Schedule[59] = s1(Schedule[57]) + Schedule[52] + s0(Schedule[44]) + Schedule[43];
-    Schedule[60] = s1(Schedule[58]) + Schedule[53] + s0(Schedule[45]) + Schedule[44];
-    Schedule[61] = s1(Schedule[59]) + Schedule[54] + s0(Schedule[46]) + Schedule[45];
-    Schedule[62] = s1(Schedule[60]) + Schedule[55] + s0(Schedule[47]) + Schedule[46];
-    Schedule[63] = s1(Schedule[61]) + Schedule[56] + s0(Schedule[48]) + Schedule[47];
 
     std::uint32_t a = Result[0];
     std::uint32_t b = Result[1];
@@ -114,20 +84,25 @@ static void update (std::uint32_t* Result, const std::uint8_t* From) noexcept
     std::uint32_t g = Result[6];
     std::uint32_t h = Result[7];
 
-    for(std::uint8_t Iter = 0x0; Iter < 64; Iter += 1)
-    {
-        const std::uint32_t tmp1 = h + S1(e) + ch(e, f, g) + Constants[Iter] + Schedule[Iter];
-        const std::uint32_t tmp2 = S0(a) + maj(a, b, c);
-        
-        h = g;
-        g = f;
-        f = e;
-        e = d + tmp1;
-        d = c;
-        c = b;
-        b = a;
-        a = tmp1 + tmp2;
-    }
+    std::uint32_t tmp1;
+    std::uint32_t tmp2;
+
+    Round0_15(0); Round0_15(1); Round0_15(2); Round0_15(3);
+    Round0_15(4); Round0_15(5); Round0_15(6); Round0_15(7);
+    Round0_15(8); Round0_15(9); Round0_15(10); Round0_15(11);
+    Round0_15(12); Round0_15(13); Round0_15(14); Round0_15(15);
+    Round16_63(16); Round16_63(17); Round16_63(18); Round16_63(19);
+    Round16_63(20); Round16_63(21); Round16_63(22); Round16_63(23);
+    Round16_63(24); Round16_63(25); Round16_63(26); Round16_63(27);
+    Round16_63(28); Round16_63(29); Round16_63(30); Round16_63(31);
+    Round16_63(32); Round16_63(33); Round16_63(34); Round16_63(35);
+    Round16_63(36); Round16_63(37); Round16_63(38); Round16_63(39);
+    Round16_63(40); Round16_63(41); Round16_63(42); Round16_63(43);
+    Round16_63(44); Round16_63(45); Round16_63(46); Round16_63(47);
+    Round16_63(48); Round16_63(49); Round16_63(50); Round16_63(51);
+    Round16_63(52); Round16_63(53); Round16_63(54); Round16_63(55);
+    Round16_63(56); Round16_63(57); Round16_63(58); Round16_63(59);
+    Round16_63(60); Round16_63(61); Round16_63(62); Round16_63(63);
 
     Result[0] += a;
     Result[1] += b;
@@ -139,12 +114,15 @@ static void update (std::uint32_t* Result, const std::uint8_t* From) noexcept
     Result[7] += h;
 }
 
-[[ nodiscard ]] std::uint32_t* raw_sha256(const void* Data, std::uint64_t Length) noexcept
+[[ nodiscard ]]
+std::uint32_t* raw_sha256(const void* Data, std::uint64_t Length) noexcept
 {
     std::uint64_t Iter = 0x0;
     const std::uint64_t ReservedLength = 64 * ((Length + 8) / 64 + 1);
     
-    std::uint32_t* Result = (std::uint32_t*)crp_allocate(32);
+    std::uint32_t* Result = static_cast<std::uint32_t*>(crp_allocate(32));
+    std::uint8_t* MessageBlock = static_cast<std::uint8_t*>(alloca(ReservedLength));
+
     Result[0] = Initial[0];
     Result[1] = Initial[1];
     Result[2] = Initial[2];
@@ -154,7 +132,6 @@ static void update (std::uint32_t* Result, const std::uint8_t* From) noexcept
     Result[6] = Initial[6];
     Result[7] = Initial[7];
 
-    std::uint8_t* MessageBlock = (std::uint8_t*)alloca(ReservedLength);
     std::memset(MessageBlock + Length, 0x0, ReservedLength);
     std::memcpy(MessageBlock, static_cast<const std::uint8_t*>(Data), Length);
     MessageBlock[Length] = (std::uint8_t)0b10000000;
@@ -168,13 +145,14 @@ static void update (std::uint32_t* Result, const std::uint8_t* From) noexcept
     MessageBlock[ReservedLength - 6] = 255 & (Length >> 40);
     MessageBlock[ReservedLength - 7] = 255 & (Length >> 48);
     MessageBlock[ReservedLength - 8] = 255 & (Length >> 56);
-    
+
     for(Iter = 0x0; Iter < ReservedLength; Iter += 64)
         update(Result, MessageBlock + Iter);
 
     return Result;
 }
 
+[[ nodiscard ]]
 static std::string symbol_to_hex(const std::uint8_t x, const CRP_FORMAT Format)
 {
     std::string String;
@@ -191,6 +169,7 @@ static std::string symbol_to_hex(const std::uint8_t x, const CRP_FORMAT Format)
     return String;
 }
 
+[[ nodiscard ]]
 std::string crp_to_hex(const std::uint32_t* HexData, std::uint64_t HexLength, const std::string& Delimiter, const CRP_FORMAT Format) noexcept
 {
     std::string Result;
@@ -222,10 +201,11 @@ std::string crp_to_hex(const std::uint32_t* HexData, std::uint64_t HexLength, co
     return Result;
 }
 
+[[ nodiscard ]]
 std::string sha256(const void* Data, const std::uint64_t Length, const std::string Delimiter, const CRP_FORMAT Format) noexcept
 {
     std::uint32_t* __Result = raw_sha256(Data, Length);
-    std::string Result = crp_to_hex(__Result, CRP_SHA256_32BYTES_BLOCKS_COUNT, Delimiter, Format);
+    const std::string Result = crp_to_hex(__Result, CRP_SHA256_32BYTES_BLOCKS_COUNT, Delimiter, Format);
 
     crp_free(__Result);
     return Result;
